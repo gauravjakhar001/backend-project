6320be81d9467be48489246177cf5e5bbde652dc
Gaurav Jakhar <jakhargaurav001@gmail.com>
2025-05-11 10:48:10 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -8,8 +8,21 @@ dotenv.config({
 })
 
 
+//jb bhi asynchronously method jb bhi complete hota hai to 
+// vo hme promise return krta hai 
+// to hm yha opr .then and .catch ka use kr skte hai 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is runing at port : ${process.env.PORT}`)
+    })
+
+})
+.catch((err)=>{
+    console.log("MONGO db Connection failed!!",err)
+
+})
 
  
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+
+    constructor(
+        statuscCode,
+        message = "Somethinf went wrong",
+        errors = [],
+        stack = ""
+
+    ) {
+        super(message)
+        this.statuscCode = statuscCode
+        this.data = null 
+        this.message  = message
+        this.success = false
+        this.errors = errors
+
+        if(stack){
+            this.stack  = stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export { ApiError } ;
